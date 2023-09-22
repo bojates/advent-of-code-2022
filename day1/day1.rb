65a717667b6f29fe998a13c7199ef28d13a7d478
Jemima Kingsley <jemima@bojates.com>
2023-09-22 16:17:26 +0100

    Day 1, challenge 2, and refactor for challenge 1

--- day1/day1.rb
+++ day1/day1.rb
@@ -1,21 +1,36 @@
-def calculate_max_calories(filename)
+def calculate_max_calories(filename, number_eleves = 1)
+  filename = File.dirname(__FILE__) + '/' + filename
+  
+  totals = []
   sum = 0
-  max = 0
-
+  
   File.foreach(filename) do |line| 
     sum += line.to_i
-    
+
     if line =~ /^([^0-9]*)$/
-      max = sum if sum > max
+      totals << sum
       sum = 0
+    elsif line == File.readlines(filename).last
+      totals << sum 
     end
+
   end
 
-  max
+  totals.sort!.reverse!
+  totals.first(number_eleves).sum
 end
 
-puts 'PASS' if calculate_max_calories('calories_test.txt') == 24000
-puts calculate_max_calories('calories_test.txt') # 24000
-puts ''
-puts 'PASS' if calculate_max_calories('calories.txt') == 71502
-puts calculate_max_calories('calories.txt')
+def test(calculation, expected)
+  puts calculation == expected ? 'PASS' : 'FAIL'
+  puts calculation
+  puts ''
+end
+
+# Tests
+test(calculate_max_calories('calories_test.txt'), 24000)
+test(calculate_max_calories('calories.txt'), 71502)
+test(calculate_max_calories('calories_test.txt', 3), 45000)
+
+# Results
+puts calculate_max_calories('calories.txt', 3) 
+
