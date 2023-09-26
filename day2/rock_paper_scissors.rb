0e581df2539302adcc226f2713dbdf069ca475d2
Jemima Kingsley <jemima@bojates.com>
2023-09-26 11:33:21 +0100

    Tidy file

--- day2/rock_paper_scissors.rb
+++ day2/rock_paper_scissors.rb
@@ -1,98 +1,5 @@
-=begin
-# Problem
-  Rock Paper Scissors
-  Multiple rounds
-  rock > scissors
-  scissors > paper
-  paper > rock
-
-  Strategy guide
-  first column: opponent
-  second column: proposed play <- I care about this player winning
-  
-  Them: 
-  A = rock
-  B = paper
-  C = scissors
-
-  Me
-  X = rock
-  Y = paper
-  Z = scissors
-
-  Total score is sum of rounds.
-  Single round is shape + outcome
-  1 = rock (X)
-  2 = paper (Y)
-  3 = scissors (Z)
-  plus
-  0 = lost
-  3 = draw
-  6 = won
-
-# Example
-A Y
-B X
-C Z
-
-Rock vs Paper. I win. Paper = 2, win = 6, total 8. 
-Paper vs rock. Lose. 1. 
-Scissors vs scissors. Draw. 3 + 3. 6. 
-Total: 15
-
-# Data
-Input: puzzle guide. Text format. One round per line. Space separated plays. 
-?? Handle bad input? 
-Check we are including the last line. 
-Output: single score. 
-
-# Algorithm
-Start a score of 0
-Constants
-  RPS { 'X' => 'rock', etc.}
-  PLAY_SCORE { 'rock' => 1, etc. }
-  RESULT_SCORE = { 'win' => 6, etc. }
-
-Read the strategy guide. 
-  - open file
-  - create an iterator
-  - loop through the file
-For each line: 
-  calculate the result (win, lose, draw)
-    - split the line using the space? Probably better use regexp to grab ABC or XYZ
-    - round_result(them, me) returns (win, lose, draw)
-    - win / lose / draw score + my_play_score
-  add to a total score
-Return the score. 
-
-round_result(their play, my_play)
-  switch their_play
-  rock
-    my_play == rock
-      draw
-    my_play == paper
-      win
-    my_play == scissors
-      lose
-  paper
-    my_play == rock
-      lose
-    my_play == paper
-      draw
-    my_play == scissors
-      win
-  scissors
-    my_play == rock
-      win
-    my_play == paper
-      lose
-    my_play == scissors
-      draw
-
-=end
-
 class RPS
-  CODE_TO_WORD = { 'A' => 'rock', 'B' => 'paper', 'C' => 'scissors' }
+  CODE_TO_PLAY = { 'A' => 'rock', 'B' => 'paper', 'C' => 'scissors' }
   CODE_TO_RESULT = { 'X' => 'lose', 'Y' => 'draw', 'Z' => 'win' }
   PLAY_SCORE = { 'rock' => 1, 'paper' => 2, 'scissors' => 3 }
   RESULT_SCORE = { 'lose' => 0, 'draw' => 3, 'win' => 6 }
@@ -106,7 +13,6 @@ class RPS
       their_play, desired_result = decode_line(line)
       total_score += RESULT_SCORE.fetch(desired_result)
       total_score += PLAY_SCORE.fetch(my_play(their_play, desired_result))
-      # total_score += PLAY_SCORE.fetch(me)
     end
 
     total_score
@@ -122,7 +28,7 @@ class RPS
 
   def self.decode_line(line)
     _, player_1, result = *line.match(/([ABC])\s([XYZ])/)
-    [CODE_TO_WORD.fetch(player_1), CODE_TO_RESULT.fetch(result)]
+    [CODE_TO_PLAY.fetch(player_1), CODE_TO_RESULT.fetch(result)]
   end
 
   def self.single_play(their_play, my_play)
@@ -144,14 +50,11 @@ class RPS
 
 end
 
-
 def run_test(test_result, expected)
   puts test_result == expected ? 'PASS' : '** FAIL ** '
   puts 'result: ' + test_result.to_s
   puts 
 end
 
-# puts RPS.play_series('strategy_guide_test.txt')
-
 run_test(RPS.play_series('strategy_guide_test.txt'), 12)
-run_test(RPS.play_series('strategy_guide.txt'), 0)
+run_test(RPS.play_series('strategy_guide.txt'), 14652)
