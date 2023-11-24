451396f9bec007d1cb5acb58797d3c29f4d71372
Jemima Kingsley <jemima@bojates.com>
2023-11-24 16:52:07 +0000

    Standard changes to ruby file formatting

--- day1/day1.rb
+++ day1/day1.rb
@@ -1,19 +1,19 @@
 def calculate_max_calories(filename, number_elves = 1)
-  filename = File.dirname(__FILE__) + '/' + filename
+  filename = File.dirname(__FILE__) + "/" + filename
   # create an iterator to avoid loading the whole file into memory
   # but also so we can check for the end of the file as we loop
-  lines = File.foreach(filename) 
-  
+  lines = File.foreach(filename)
+
   totals = []
   sum = 0
-  
-  lines.each_with_index do |line, idx| 
+
+  lines.each_with_index do |line, idx|
     sum += line.to_i
-    if line =~ /^([^0-9]*)$/
+    if /^([^0-9]*)$/.match?(line)
       totals << sum
       sum = 0
     elsif idx == lines.count - 1
-      totals << sum 
+      totals << sum
     end
   end
 
@@ -22,17 +22,17 @@ def calculate_max_calories(filename, number_elves = 1)
 end
 
 def test(calculation, expected)
-  puts calculation == expected ? 'PASS' : 'FAIL'
+  puts (calculation == expected) ? "PASS" : "FAIL"
   puts calculation
-  puts ''
+  puts ""
 end
 
 # Tests
-test(calculate_max_calories('calories_test.txt'), 24000)
-test(calculate_max_calories('calories_test2.txt'), 24000)
-test(calculate_max_calories('calories.txt'), 71502)
-test(calculate_max_calories('calories_test.txt', 3), 45000)
-test(calculate_max_calories('calories_test2.txt', 3), 45000)
+test(calculate_max_calories("calories_test.txt"), 24000)
+test(calculate_max_calories("calories_test2.txt"), 24000)
+test(calculate_max_calories("calories.txt"), 71502)
+test(calculate_max_calories("calories_test.txt", 3), 45000)
+test(calculate_max_calories("calories_test2.txt", 3), 45000)
 
 # Results
-puts calculate_max_calories('calories.txt', 3) 
+puts calculate_max_calories("calories.txt", 3)
